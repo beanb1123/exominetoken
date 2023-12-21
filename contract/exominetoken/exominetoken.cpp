bc2ec3bd4175eea0219f8070f31afecddf315036
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2023-12-21 19:36:28 +0100

    Update exominetoken.cpp

--- contract/exominetoken/exominetoken.cpp
+++ contract/exominetoken/exominetoken.cpp
@@ -67,6 +67,9 @@ void token::mine( const name& miner ) {
     }
 }
 
+         [[eosio::action]]
+         void log(std::string message);
+
 void token::create( const name&   issuer,
                     const asset&  maximum_supply )
 {
